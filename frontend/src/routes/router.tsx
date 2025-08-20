13cf8adc5aec4ffc550419783f147e6620c369da
Jung-Eon <egg9054@gmail.com>
2025-08-20 20:57:28 +0900

    Feat:초기 화면 구조 세팅

---
+++ frontend/src/routes/router.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "@/App";
+import LoginPage from "@/pages/login/LoginPage";
+import MapPage from "@/pages/map/MapPage";
+import WritePage from "@/pages/write/WritePage";
+import LetterPage from "@/pages/letter/LetterPage";
+import MyPage from "@/pages/mypage/MyPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <LoginPage /> }, // 시작점: 로그인
+      { path: "map", element: <MapPage /> },
+      { path: "write", element: <WritePage /> },
+      { path: "letter/:id", element: <LetterPage /> },
+      { path: "mypage", element: <MyPage /> },
+    ],
+  },
+]);
+
+export default router;
