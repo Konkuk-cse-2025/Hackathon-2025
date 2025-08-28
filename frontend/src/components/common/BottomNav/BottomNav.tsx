d3342e30c5d1519616a0b5a6971b3a404de2dcbe
Jung-Eon <egg9054@gmail.com>
2025-08-29 00:09:05 +0900

    svg누락 해결

--- frontend/src/components/common/BottomNav/BottomNav.tsx
+++ frontend/src/components/common/BottomNav/BottomNav.tsx
@@ -10,7 +10,7 @@ export default function BottomNav() {
           isActive ? styles.active : styles.inactive
         }
       >
-        <img src="icons/mapLogo.svg" className={styles.icon} />
+        <img src="icons/map.svg" className={styles.icon} />
       </NavLink>
       <NavLink
         to="/mypage"
@@ -18,7 +18,7 @@ export default function BottomNav() {
           isActive ? styles.active : styles.inactive
         }
       >
-        <img src="icons/myLogo.svg" className={styles.profileIcon} />
+        <img src="icons/profile.svg" className={styles.profileIcon} />
       </NavLink>
     </nav>
   );
