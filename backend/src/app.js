161b87f34e6b23f8d10d59895d6a41e1d64dbf9f
vourove <yuujeong0816@naver.com>
2025-08-28 16:06:56 +0900

    fix: include backend package files

--- backend/src/app.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-
-app.get("/", (_req, res) => res.send("OK"));
-
-module.exports = app;
--- backend/src/routes/index.js
+++
@@ -1,4 +0,0 @@
-const r = require('express').Router();
-r.use('/mailboxes', require('./mailbox.route'));
-r.use('/letters', require('./letter.route'));
-module.exports = r;
